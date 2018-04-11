e8bc18268ace16dc47e11c5aea62910d9fc6548d
K1kk3rt <37216006+K1kk3rt@users.noreply.github.com>
2018-04-11 18:21:58 +0200

    Add files via upload

---
+++ PraktijkProgrammeren1-OefenTentamen/Anagram/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef woord: ");
+            string woord1 = Console.ReadLine();
+
+            Console.Write("Geef een ander woord: ");
+            string woord2 = Console.ReadLine();
+
+            bool anagram = woord1.Length == woord2.Length;
+
+            int pos = 0;
+
+            string test = woord2;
+
+            int index = 0;
+
+            while ((index < woord1.Length) && (anagram)) {
+
+                pos = test.IndexOf(woord1[index]);
+
+                if (pos >= 0)
+                {
+                    test = test.Remove(pos, 1);
+                    index++;
+                }
+                else
+                {
+                    anagram = false;
+                }
+            }
+
+            if (anagram)
+            {
+                Console.WriteLine("'" + woord2 + "'" + " is een anagram van " + "'" + woord1 + "'" );
+            }
+            else
+            {
+                Console.WriteLine("'" + woord2 + "'" + " is geen anagram van " + "'" + woord1 + "'");
+            }
+
+            Console.ReadKey();
+       
+        }
+    }
+}
---
+++ PraktijkProgrammeren1-OefenTentamen/Schruifpuzzel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schruifpuzzel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int stukjes = new int[2, 4, 3, 1, 5, 6, 7, 8, -1];
+
+            int legePos = 6;
+
+            //TekenPuzzel(stukjes);  METHOD
+
+
+        }
+    }
+}
